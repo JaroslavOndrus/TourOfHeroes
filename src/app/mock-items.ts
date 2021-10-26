da74705aa12ecf59e76d8df8f876fecca9af5a61
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-10-26 10:00:57 +0200

    add items to hero interface

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,12 +1,14 @@
 import { Item } from "./item";
 
-export const ITEMS: Item[] = [
-    { id: 11, name: "Jablko", price: 100},
-    { id: 12, name: "Hruska", price: 652},
-    { id: 13, name: "Palica", price: 158},
-    { id: 14, name: "Bicykel", price: 1100},
-    { id: 15, name: "Auto", price: 28},
-    { id: 16, name: "Motorka", price: 3},
-    { id: 17, name: "Sekera", price: 1},
+export const Items: Item[] = [
+    { id: 11, name: 'Stone Hammer', price: 100}, //0
+    { id: 12, name: 'Stone Axe', price: 652},
+    { id: 13, name: 'Wooden Stick', price: 158},
+    { id: 14, name: 'Knife', price: 1100},
+    { id: 15, name: 'Sword', price: 28},
+    { id: 16, name: 'Butcher Knife', price: 3},
+    { id: 17, name: 'Machette', price: 1},
     { id: 18, name: "Kameň", price: 2},
+    { id: 19, name: 'Axe', price: 150},
+    { id: 20, name: 'Hammer', price: 120}, //9
 ]
