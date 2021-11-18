43ffb4fe5c27cf6e3b3e7c6c30a3dab30dcaf88b
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-11-18 18:03:54 +0100

    Create new mock items list

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -12,3 +12,16 @@ export const Items: Item[] = [
     { id: 19, name: 'Axe', price: 150},
     { id: 20, name: 'Hammer', price: 120}, //9
 ]
+
+export const ITEMS: Item[] = [
+    { id: 21, name: 'Stone Hammer', price: 1}, //0
+    { id: 22, name: 'Stone Axe', price: 2},
+    { id: 23, name: 'Wooden Stick', price: 3},
+    { id: 24, name: 'Knife', price: 4},
+    { id: 25, name: 'Sword', price: 5},
+    { id: 26, name: 'Butcher Knife', price: 6},
+    { id: 27, name: 'Machette', price: 7},
+    { id: 28, name: "Kameň", price: 8},
+    { id: 29, name: 'Axe', price: 9},
+    { id: 30, name: 'Hammer', price: 10}, //9
+]
