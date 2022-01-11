faaa5ecc6627b6298f3ccc22b792dd9b0821be2e
Jaroslav Ondruš <jaroslavondrus04@gmail.com>
2022-01-11 08:42:00 +0100

    add more elements from materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ItemSearchComponent } from './item-search/item-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule} from '@angular/material/button';
-
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -36,6 +36,7 @@ import { MatButtonModule} from '@angular/material/button';
   ],
   imports: [
     BrowserModule,
+    MatInputModule,
     MatButtonModule,
     MatSliderModule,
     FormsModule,
